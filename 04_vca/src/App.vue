1a9cd5f3483475594735b4b4bfea0fe741be477c
夕豆 <shadow@shadow.local>
2023-05-12 13:35:19 +0800

    learning

---
+++ 04_vca/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <h2>{{ gretting }}</h2>
+  <h2>obj.greeting: {{ obj.gretting.value }}</h2>
+  <h2>info.gretting: {{ info.gretting }}</h2>
+  <h2>info: {{ info.name }}</h2>
+  <h2>readonlyInfo: {{ readonlyInfo.name }}</h2>
+  <button v-on:click="assignInfo">AssignInfo</button>
+  <home :readonlyInfo="readonlyInfo"></home>
+</template>
+
+<script>
+import Home from '@/components/Home.vue'
+import { reactive, readonly, ref } from 'vue'
+
+export default {
+  components: {
+    Home
+  },
+  setup() {
+    const gretting = ref('Hello, Vue Composition API!')
+    const obj = { gretting }
+    const info = reactive({ name: 'shadow', age: 1, gretting })
+    const readonlyInfo = readonly(info)
+
+    const assignInfo = () => {
+      info.name = 'iris'
+    }
+
+    return {
+      gretting,
+      obj,
+      info,
+      readonlyInfo,
+      assignInfo
+    }
+  }
+}
+</script>
